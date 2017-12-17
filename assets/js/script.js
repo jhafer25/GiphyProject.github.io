c7fb2734c3c2344ccb4234aa98fe3d4be19da9e1
James Hafer <jhafer25@gmail.com>
2017-12-17 11:08:33 -0700

    Complet Giphy Generator

---
+++ assets/js/script.js
@@ -0,0 +1,58 @@
+$('document').ready(function(){
+	let giphies = ["Puppies", "Food Porn", "Leslie Knope", "Fail", "GOT", "Winter", "Rihanna", "Socks", "Trending", "Joy"];
+	let searchTag = giphies[0];
+	let queryURL = `http://api.giphy.com/v1/gifs/search?q=${searchTag}&api_key=dc6zaTOxFJmzC&limit=9`;
+	
+	$(document).on('click', "button.giphyButton", function() {
+      	searchTag = $(this).html();
+      	queryURL = `http://api.giphy.com/v1/gifs/search?q=${searchTag}&api_key=dc6zaTOxFJmzC&limit=9`
+      	console.log(searchTag);
+      	callGiphy();
+    });
+
+    function callGiphy(){
+		$.ajax({
+        	url: queryURL,
+        	method: "GET"
+      	}).done(function(response) {
+      		console.log(response);
+      		// console.log(response.data[0].url);
+
+      		for(let i = 0; i < response.data.length; i++){
+      			let imgURL = response.data[i].images.original.url;
+      			let linkURL = response.data[i].url;
+      			let gifTitle = response.data[i].title;
+      			console.log(imgURL);
+      			$('#randomImg-'+[i]).attr("src",imgURL);
+      			$('#randomTitle-'+[i]).html(gifTitle);
+      			$('#giphyLink-'+[i]).attr("href",linkURL);
+      		}
+		});
+	}
+
+    // Function for displaying giphy data
+    function renderButtons() {
+        $("#button-container").empty();
+        for(let index = 0; index < giphies.length; index++){
+          	let button = $("<button>");
+          	button.html(giphies[index]);
+          	button.addClass("giphyButton btn btn-primary");
+          	$("#button-container").append(button);
+        }
+
+    }
+
+      // This function handles events where one button is clicked
+    $("#add-giphy").on("click", function(event) {
+        event.preventDefault();
+        let giphyTextField = $("#giphy-input").val();
+        if(giphyTextField){
+          	giphies.push(giphyTextField);
+          	renderButtons();
+          	$('#giphy-input').val("");
+        }
+   	});
+
+    renderButtons();
+    callGiphy();
+});
